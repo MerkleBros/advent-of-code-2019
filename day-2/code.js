f25920ae0ac8dee4dff9f7d0a8381a4485ccbce1
Patrick McCarver <patrick@merklebros.com>
2019-12-03 14:15:04 -0500

    Day 2 problems in Javascript

---
+++ day-2/code.js
@@ -0,0 +1,79 @@
+const input =
+  [
+    1, 0, 0, 3, 1, 1, 2, 3, 1, 3, 4, 3, 1, 5, 0, 3, 2, 10, 1, 19, 1, 6,
+    19, 23, 1, 23, 13, 27, 2, 6, 27, 31, 1, 5, 31, 35, 2, 10, 35, 39,
+    1, 6, 39, 43, 1, 13, 43, 47, 2, 47, 6, 51, 1, 51, 5, 55, 1, 55, 6,
+    59, 2, 59, 10, 63, 1, 63, 6, 67, 2, 67, 10, 71, 1, 71, 9, 75, 2,
+    75, 10, 79, 1, 79, 5, 83, 2, 10, 83, 87, 1, 87, 6, 91, 2, 9, 91,
+    95, 1, 95, 5, 99, 1, 5, 99, 103, 1, 103, 10, 107, 1, 9, 107, 111,
+    1, 6, 111, 115, 1, 115, 5, 119, 1, 10, 119, 123, 2, 6, 123, 127,
+    2, 127, 6, 131, 1, 131, 2, 135, 1, 10, 135, 0, 99, 2, 0, 14, 0
+  ]
+
+let state = [...input]
+
+const resetState = (noun, verb) => {
+  state = [...input]
+  state[1] = noun
+  state[2] = verb
+}
+
+const traverseInput = (startIndex, windowSize) => {
+  if(startIndex >= state.length) return
+  const exitCode = performOperation(startIndex, startIndex + windowSize)
+  if (exitCode === 99) return
+  traverseInput(startIndex + windowSize, windowSize)
+}
+
+const performOperation = (startIndex, windowSize) => {
+
+  const opCode = state[startIndex]
+  const operandOne = state[startIndex + 1]
+  const operandTwo = state[startIndex + 2]
+  const destination = state[startIndex + 3]
+  let result
+  if(opCode === 99) return 99
+  if(opCode === 1) {
+    result = state[operandOne] + state[operandTwo]
+  }
+  if(opCode === 2) {
+    result = state[operandOne] * state[operandTwo]
+  }
+  state[destination] = result 
+  return 1
+}
+
+const findNounAndVerb = (target) => {
+  for(let i = 0; i < 100; i++) {
+    for(let j = 0; j < 100; j++) {
+      resetState(i, j)
+      traverseInput(0, 4)
+      if(state[0] === target) {return [i, j]}
+    }
+  }
+  return []
+}
+
+// tests
+state = [1,0,0,0,99]
+traverseInput(0, 4)
+console.log(state)
+
+state = [2,3,0,3,99]
+traverseInput(0, 4)
+console.log(state)
+
+state = [2,4,4,5,99,0]
+traverseInput(0, 4)
+console.log(state)
+
+state = [1,1,1,4,99,5,6,0,99]
+traverseInput(0, 4)
+console.log(state)
+
+resetState(12, 2)
+traverseInput(0, 4)
+console.log("Final result: ", state[0])
+
+let [noun,verb] = findNounAndVerb(19690720)
+console.log(100*noun + verb)
